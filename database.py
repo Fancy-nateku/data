f742e0df4c766aa5e104e30b68c96f80c67aae11
Fancy-nateku <fancymegiri01@gmail.com>
2025-09-26 13:00:23 +0300

    Load database URL from environment variables

--- database.py
+++ database.py
@@ -1,20 +1,19 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:Grok2025!@localhost:3306/farm_expense_tracker"
+load_dotenv()  # Load from .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-#Dependency to get DB session in FastAPI endpoints
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
